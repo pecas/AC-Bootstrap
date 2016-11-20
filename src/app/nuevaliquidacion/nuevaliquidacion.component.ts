bb40f8b84461fe2779b13750f14904a6ef7c5479
Eduardo Alonso <alonsoeduardo@gmail.com>
2016-11-20 13:50:33 -0300

    Reparacion Componentes

--- src/app/nuevaliquidacion/nuevaliquidacion.component.ts
+++ src/app/nuevaliquidacion/nuevaliquidacion.component.ts
@@ -1,21 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { LiquidacionesService } from '../services/liquidaciones.service';
-import { Liquidacion } from '../Model/liquidacion.model';
-import { Router, routerparams } from '@angular/router';
 
 @Component({
   selector: 'app-nuevaliquidacion',
   templateUrl: './nuevaliquidacion.component.html',
-  styleUrls: ['./nuevaliquidacion.component.css'],
-   providers: [LiquidacionesService]
+  styleUrls: ['./nuevaliquidacion.component.css']
 })
 export class NuevaliquidacionComponent implements OnInit {
-  public Liquidaciones: Liquidacion;
+
   constructor() { }
 
   ngOnInit() {
-    this.Liquidaciones = new Liquidacion();
-
   }
 
 }
